a72ad2d13d7ed1ecd92f9e5a67e30dc611f40317
evagarcia24 <evagsegovia@hotmail.com>
2026-02-11 01:06:34 +0100

    eliminado del front los schema oracle porque esto se hace en el back

--- src/main/resources/db/migration/V04__insert_initial_packs.sql
+++
@@ -1,7 +0,0 @@
--- V04__insert_initial_packs.sql
-
-INSERT INTO pack (pack_id, version, created_at, description)
-VALUES ('democracia', NULL, SYSTIMESTAMP, NULL);
-
-INSERT INTO pack (pack_id, version, created_at, description)
-VALUES ('misterio', NULL, SYSTIMESTAMP, NULL);
